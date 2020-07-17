9735d46fc4ef2f9b1dbfb6dddb36b3635db0c35d
Anastasia Feldman <anst.feldman@gmail.com>
2020-07-17 09:14:52 +0300

    Revert "added cycles to ask user"
    This reverts commit 1fe96ed139216ab4c2bb3fc7ea5685931cc78de8.

--- movies.js
+++ movies.js
@@ -7,28 +7,11 @@ let personalMovieDB = {
   genre: [],
   private: false
 };
-
-for (let i = 0; i < numberOfFilms; i++) {
-  let movieName = prompt("Один из последних просмотренных фильмов?", "");
-  while (movieName === null || movieName.length === 0 || movieName.length > 50) {
-    movieName = prompt("Один из последних просмотренных фильмов?", "");
-  } 
-  let movieRate = +prompt("На сколько оцените его?", "");
-  while (movieRate === null || movieRate.length === 0 || movieRate.length > 50) {
-    movieRate = +prompt("На сколько оцените его?", "");
-    personalMovieDB.movies[movieName] = movieRate;  
-  }
-    
-}
-
-if (personalMovieDB.count < 10) {
-  alert('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count <= 30) {
-  alert('Вы классический зритель');
-} else if (personalMovieDB.count > 30) {
-  alert('Вы киноман');
-} else {
-  alert('Произошла ошибка');
-}
+let movieName = prompt("Один из последних просмотренных фильмов?", "");
+let movieRate = +prompt("На сколько оцените его?", "");
+let movieName2 = prompt("Один из последних просмотренных фильмов?", "");
+let movieRate2 = +prompt("На сколько оцените его?", "");
+personalMovieDB.movies[movieName] = movieRate;
+personalMovieDB.movies[movieName2] = movieRate2;
 
 console.log(personalMovieDB);
